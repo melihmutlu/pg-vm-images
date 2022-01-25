615e809857efb6bab128e2dc97c6c05d0d052afc
Melih Mutlu <memutlu@microsoft.com>
2022-01-25 16:32:27 +0300

    added test script

--- scripts/windows_install_mingw64.ps1
+++ scripts/windows_install_mingw64.ps1
@@ -9,16 +9,25 @@ if (!$?) { throw 'cmdfail' }
 Remove-Item msys2.exe ;
 
 
-function msys() { C:\msys64\usr\bin\bash.exe @('-lc') + @Args; } ; `
-msys ' ' ; `
-msys 'pacman --noconfirm -Syuu' ; `
-msys 'pacman --noconfirm -Scc' ; `
-echo 'installing packages' ; `
-msys 'pacman --noconfirm --needed -S mingw-w64-x86_64-toolchain base-devel' ; `
-echo 'export PATH=$PATH:/c/msys64/mingw64/bin' >> C:\msys64\etc\profile ; `
-msys 'source /etc/profile' `
+function msys() { C:\msys64\usr\bin\bash.exe @('-lc') + @Args; } ; 
+msys ' ' ; 
+msys 'pacman --noconfirm -Syuu' ; 
+msys 'pacman --noconfirm -Scc' ; 
+
+echo 'installing packages' ; 
+msys 'pacman --noconfirm --needed -S base-devel \
+    mingw-w64-x86_64-toolchain \
+    mingw-w64-x86_64-icu \
+    mingw-w64-x86_64-openldap \
+    mingw-w64-x86_64-libxml2 \
+    mingw-w64-x86_64-libxslt \
+    mingw-w64-x86_64-lz4' ; 
+echo 'export PATH=$PATH:/c/msys64/mingw64/bin' >> C:\msys64\etc\profile ; 
+msys 'source /etc/profile' 
 
 # adding pexports for mingw since it's not included in msys
-msys 'curl.exe -sSL -o pexports.tar.xz  https://netcologne.dl.sourceforge.net/project/mingw/MinGW/Extension/pexports/pexports-0.47/pexports-0.47-mingw32-bin.tar.xz' `
-msys 'tar xJvf pexports.tar.xz' `
-msys 'cp ./bin/pexports.exe /c/msys64/mingw64/bin/pexports.exe'
+echo 'installing pexports' ;
+Invoke-WebRequest -UseBasicParsing -uri "https://netcologne.dl.sourceforge.net/project/mingw/MinGW/Extension/pexports/pexports-0.47/pexports-0.47-mingw32-bin.tar.xz" -OutFile pexports.tar.xz ;
+if (!$?) { throw 'cmdfail' }
+msys 'tar xJvf /c/pexports.tar.xz' ;
+
