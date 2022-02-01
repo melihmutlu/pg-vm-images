abd2a9767af85f2ebbe53980c3a5f621040d84a7
Melih Mutlu <memutlu@microsoft.com>
2022-02-01 11:47:06 +0300

    addressed reviews

--- scripts/windows_install_mingw64.ps1
+++ scripts/windows_install_mingw64.ps1
@@ -2,7 +2,7 @@ $ErrorActionPreference = "Stop"
 
 echo "starting mingw64 installation"
 
-Invoke-WebRequest -UseBasicParsing -uri "https://github.com/msys2/msys2-installer/releases/download/nightly-x86_64/msys2-base-x86_64-latest.sfx.exe" -OutFile msys2.exe ; 
+curl.exe -L "https://github.com/msys2/msys2-installer/releases/download/nightly-x86_64/msys2-base-x86_64-latest.sfx.exe" -o msys2.exe
 if (!$?) { throw 'cmdfail' }
 
 .\msys2.exe -y -oC:\ ;
@@ -10,24 +10,23 @@ Remove-Item msys2.exe ;
 
 
 function msys() { C:\msys64\usr\bin\bash.exe @('-lc') + @Args; } ; 
+if (!$?) { throw 'cmdfail' }
+
 msys ' ' ; 
 msys 'pacman --noconfirm -Syuu' ; 
 msys 'pacman --noconfirm -Scc' ; 
 
 echo 'installing packages' ; 
-msys 'pacman --noconfirm --needed -S base-devel \
-    mingw-w64-x86_64-toolchain \
-    mingw-w64-x86_64-icu \
-    mingw-w64-x86_64-openldap \
-    mingw-w64-x86_64-libxml2 \
-    mingw-w64-x86_64-libxslt \
-    mingw-w64-x86_64-lz4' ; 
+msys 'pacman -S git bison flex make diffutils\
+    ucrt64/mingw-w64-ucrt-x86_64-perl \
+    ucrt64/mingw-w64-ucrt-x86_64-gcc \
+    ucrt64/mingw-w64-ucrt-x86_64-ccache \
+    ucrt64/mingw-w64-ucrt-x86_64-zlib \
+    ucrt64/mingw-w64-ucrt-x86_64-icu \
+    ucrt64/mingw-w64-ucrt-86_64-openldap \
+    ucrt64/mingw-w64-ucrt-x86_64-libxml2 \
+    ucrt64/mingw-w64-ucrt-x86_64-libxslt \
+    ucrt64/mingw-w64-ucrt-x86_64-lz4 \
+    ucrt64/mingw-w64-ucrt-x86_64-pkg-config ; 
 echo 'export PATH=$PATH:/c/msys64/mingw64/bin' >> C:\msys64\etc\profile ; 
 msys 'source /etc/profile' 
-
-# adding pexports for mingw since it's not included in msys
-echo 'installing pexports' ;
-Invoke-WebRequest -UseBasicParsing -uri "https://netcologne.dl.sourceforge.net/project/mingw/MinGW/Extension/pexports/pexports-0.47/pexports-0.47-mingw32-bin.tar.xz" -OutFile pexports.tar.xz ;
-if (!$?) { throw 'cmdfail' }
-msys 'tar xJvf /c/pexports.tar.xz' ;
-
