3c0c94c451818dc22fc37a4826717107936b3cd4
Melih Mutlu <memutlu@microsoft.com>
2022-02-01 12:18:25 +0300

    new gcp

--- scripts/windows_install_mingw64.ps1
+++ scripts/windows_install_mingw64.ps1
@@ -1,6 +1,6 @@
 $ErrorActionPreference = "Stop"
 
-echo "starting mingw64 installation"
+echo 'starting mingw64 installation'
 
 curl.exe -L "https://github.com/msys2/msys2-installer/releases/download/nightly-x86_64/msys2-base-x86_64-latest.sfx.exe" -o msys2.exe
 if (!$?) { throw 'cmdfail' }
@@ -17,16 +17,16 @@ msys 'pacman --noconfirm -Syuu' ;
 msys 'pacman --noconfirm -Scc' ; 
 
 echo 'installing packages' ; 
-msys 'pacman -S git bison flex make diffutils\
+msys "pacman -S git bison flex make diffutils\
     ucrt64/mingw-w64-ucrt-x86_64-perl \
     ucrt64/mingw-w64-ucrt-x86_64-gcc \
     ucrt64/mingw-w64-ucrt-x86_64-ccache \
     ucrt64/mingw-w64-ucrt-x86_64-zlib \
     ucrt64/mingw-w64-ucrt-x86_64-icu \
-    ucrt64/mingw-w64-ucrt-86_64-openldap \
+    ucrt64/mingw-w64-ucrt-x86_64-openldap \
     ucrt64/mingw-w64-ucrt-x86_64-libxml2 \
     ucrt64/mingw-w64-ucrt-x86_64-libxslt \
     ucrt64/mingw-w64-ucrt-x86_64-lz4 \
-    ucrt64/mingw-w64-ucrt-x86_64-pkg-config ; 
-echo 'export PATH=$PATH:/c/msys64/mingw64/bin' >> C:\msys64\etc\profile ; 
-msys 'source /etc/profile' 
+    ucrt64/mingw-w64-ucrt-x86_64-pkg-config" ; 
+echo 'export PATH=$PATH:/c/msys64/ucrt64/bin:/c/msys64/mingw64/bin' >> C:\msys64\etc\profile ; 
+msys 'source /etc/profile'
