40ecdaf7687d49de95cefd3c2d4ca643df79e349
Melih Mutlu <memutlu@microsoft.com>
2022-02-02 19:29:17 +0300

    reviews

--- scripts/windows_install_mingw64.ps1
+++ scripts/windows_install_mingw64.ps1
@@ -3,30 +3,17 @@ $ErrorActionPreference = "Stop"
 echo "starting mingw64 installation"
 
 curl.exe -L "https://github.com/msys2/msys2-installer/releases/download/nightly-x86_64/msys2-base-x86_64-latest.sfx.exe" -o msys2.exe
-if (!$?) { throw 'cmdfail' }
+if (!$?) { throw 'cmdfail' };
 
 .\msys2.exe -y -oC:\ ;
+if (!$?) { throw 'cmdfail' };
 Remove-Item msys2.exe ;
 
-
-function msys() { C:\msys64\usr\bin\bash.exe @('-lc') + @Args; } ; 
-if (!$?) { throw 'cmdfail' }
-
+function msys() { C:\msys64\usr\bin\bash.exe @('-lc') + @Args; if (!$?) { throw 'cmdfail' };} ; 
 msys ' ' ; 
 msys 'pacman --noconfirm -Syuu' ; 
 msys 'pacman --noconfirm -Scc' ; 
 
 echo 'installing packages' ; 
-msys 'pacman -S git bison flex make diffutils\
-    ucrt64/mingw-w64-ucrt-x86_64-perl \
-    ucrt64/mingw-w64-ucrt-x86_64-gcc \
-    ucrt64/mingw-w64-ucrt-x86_64-ccache \
-    ucrt64/mingw-w64-ucrt-x86_64-zlib \
-    ucrt64/mingw-w64-ucrt-x86_64-icu \
-    ucrt64/mingw-w64-ucrt-x86_64-openldap \
-    ucrt64/mingw-w64-ucrt-x86_64-libxml2 \
-    ucrt64/mingw-w64-ucrt-x86_64-libxslt \
-    ucrt64/mingw-w64-ucrt-x86_64-lz4 \
-    ucrt64/mingw-w64-ucrt-x86_64-pkg-config' ; 
-echo 'export PATH=$PATH:/c/msys64/ucrt64/bin:/c/msys64/mingw64/bin' >> C:\msys64\etc\profile ; 
-msys 'source /etc/profile' 
+    msys 'pacman -S --needed --noconfirm git bison flex make diffutils \
+        ucrt64/mingw-w64-ucrt-x86_64-{ccache,gcc,icu,libxml2,libxslt,lz4,openldap,perl,pkg-config,zlib}' ; 
