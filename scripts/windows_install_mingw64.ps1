7cede3c07b01332f2d7fc8d341c5a24c77de8417
Melih Mutlu <memutlu@microsoft.com>
2022-02-04 17:13:25 +0300

    remove openldap

--- scripts/windows_install_mingw64.ps1
+++ scripts/windows_install_mingw64.ps1
@@ -19,5 +19,5 @@ msys 'pacman --noconfirm -Scc' ;
 
 echo 'installing packages' ; 
 msys 'pacman -S --needed --noconfirm git bison flex make diffutils \
-    ucrt64/mingw-w64-ucrt-x86_64-{ccache,gcc,icu,libxml2,libxslt,lz4,openldap,perl,pkg-config,zlib}' ; 
+    ucrt64/mingw-w64-ucrt-x86_64-{ccache,gcc,icu,libxml2,libxslt,lz4,perl,pkg-config,zlib}' ; 
 msys 'pacman -Scc'
